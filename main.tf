5bf67f293ff8120d2d0c4da5edfc7ebd4686ca38
bornhoevd <cbornhoevd@gmail.com>
2025-01-10 14:32:22 -0800

    Added new main.tf file with Kubernetes and Helm provider configuration

---
+++ main.tf
@@ -0,0 +1,34 @@
+# Configure Kubernetes and Helm Providers
+provider "kubernetes" {
+  load_config_file = false
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host = var.kubernetes_cluster_endpoint
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command = "aws-iam-authenticator"
+    args = [
+      "token",
+      "-i",
+      "${var.kubernetes_cluster_name}"
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file = false
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host = var.kubernetes_cluster_endpoint
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command = "aws-iam-authenticator"
+      args = [
+        "token",
+        "-i",
+        "${var.kubernetes_cluster_name}"
+      ]
+    }
+  }
+}
