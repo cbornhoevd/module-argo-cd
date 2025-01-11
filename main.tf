b23d0bd8bc5fa833989ab91b1ae6e4103869d532
bornhoevd <cbornhoevd@gmail.com>
2025-01-11 02:08:45 -0800

    Added new variables.tf file and updated configuration in main.tf

--- main.tf
+++ main.tf
@@ -32,3 +32,22 @@ provider "helm" {
     }
   }
 }
+
+# Helm Chart for Argo CD Deployment
+resource "kubernetes_namespace" "argo-ns" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name = "jupiter"
+  chart = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace = "argocd"
+
+  # We are going to access the console with a port forwarded connection,
+  # so we will disable TLS. This allows us to avoid the self-signed certificate
+  # warning for localhost.
+  # controller.extraArgs = ["insecure"]
+}
